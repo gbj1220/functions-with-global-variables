943e77650e142c7efb37682253dce9305fcc9eea
Gregory.Johnson <3531op@gmail.com>
2020-10-07 10:34:32 -0400

    initial commit

--- main.test.js
+++ main.test.js
@@ -1,11 +1,37 @@
 /**********************************
  * YOUR CODE GOES HERE THIS TIME! *
  **********************************/
+let x = 3;
+let y = 10;
+let z = 4; 
 
+let greeting = 'hello';
+let firstName = 'Greg'; 
+let callStatus = 'ringing';
 
+function double() {
+  x = x * 2
+}
 
+function triple() {
+  y = y * 3
+}
 
- 
+function square() {
+  z = z * z
+}
+
+function beNice() {
+  greeting = greeting + firstName
+}
+
+function changeName() {
+  firstName = 'Danger'
+}
+
+function hangUp() {
+  callStatus = 'call ended'
+}
 
 /*********************************************************************************************************************
 
